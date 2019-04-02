8dada0104608336b33b5625bb596e926a8f6a7cf
Cyra25 <44994077+Cyra25@users.noreply.github.com>
2019-04-02 15:25:55 +0800

    numbers exercise - number 5

--- Exercises/Percentages.java
+++ Exercises/Percentages.java
@@ -1,2 +1,12 @@
 public class Percentages {
+    public static void main(String[] args){
+        double firstNum = 7.7;
+        double secondNum = 8.8;
+        computerPercent(firstNum,secondNum);
+    }
+
+    public static void computerPercent(double firstNum, double secondNum){
+        double percentage = 100*firstNum/secondNum;
+        System.out.println(firstNum+ " is "+ percentage+ " percent of "+ secondNum);
+    }
 }
---
+++ Exercises/Percentages2.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class Percentages2 {
+    public static void main(String[] args){
+        String message = JOptionPane.showInputDialog(null, "Please enter the first number");
+        double firstNum = Double.parseDouble(message);
+        String secondMessage = JOptionPane.showInputDialog(null, "Please enter second number");
+        double secondNum = Double.parseDouble(secondMessage);
+        computerPercent(firstNum,secondNum);
+    }
+
+    public static void computerPercent(double firstNum, double secondNum){
+        double percentage = 100*firstNum/secondNum;
+        JOptionPane.showMessageDialog(null, firstNum+ " is "+ percentage+ " percent of "+ secondNum);
+    }
+}
+
